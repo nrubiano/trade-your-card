33669d9adcbe5e7013f2a1a54ff3e4f52a821f89
Nicolas Rubiano <v-nicolasr@microsoft.com>
2022-09-10 23:40:14 -0500

    clean up the structure

---
+++ backend/src/TradeYouCard.API/Controllers/UsersController.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace TradeYouCard.API.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class UsersController : ControllerBase
+{
+    private readonly ILogger<UsersController> _logger;
+
+    public UsersController(ILogger<UsersController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet]
+    public IActionResult Get()
+    {
+        return Ok();
+    }
+}
